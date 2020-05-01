70f119cc2466ba16a742b29884862472b4f4b3b6
Bill Fanselow <bfanselow@gmail.com>
2020-05-01 11:18:22 -0600

    First script added

---
+++ shell_pargs.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+##
+## File: shell_pargs.sh
+## Description:
+##  Demonstate a simple way to parse command-line args in (Bourne) shell-script 
+##
+## Usage: ./shell_pargs.sh -h 
+##
+## Author: Bill Fanselow
+
+## Set Defaults
+DIR='.'
+MODE='short'
+
+##-----------------------------------------------------------------------------
+usage()
+{
+    echo "Usage:"
+    echo ""
+    echo "./simple_args_parsing.sh"
+    echo "  -h --help"
+    echo "  ls=<dir>"
+    echo "  --long"
+    echo ""
+}
+##-----------------------------------------------------------------------------
+
+## If we require some input
+if [ "$1" = "" ]; then
+   usage
+fi
+
+##-----------------------------------------------------------------------------
+## main()
+## Parse the args: key/value pairs are assumed to passed as "<key>=<val>"
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        ls)
+            DIR=$VALUE
+            ;;
+        --long)
+            MODE='long' 
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+## Perfom the script's purpose based on input options
+exec="/bin/ls $DIR"
+if [ "X${MODE}" = "Xlong" ]; then
+  exec="/bin/ls -ltr $DIR"
+fi
+$exec
+
+exit 0
